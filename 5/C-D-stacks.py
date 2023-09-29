2461961e1da600ad7e65db90e9ca5ad95c082f00
zalogarciam <gegarciam95@gmail.com>
2023-09-29 18:48:49 -0500

    CD -Stacks Teo

---
+++ 5/C-D-stacks.py
@@ -0,0 +1,29 @@
+stack = []
+
+stack.append(2)
+print(stack)
+stack.append(3)
+print(stack)
+stack.append(6)
+print(stack)
+stack.append(9) # Inserta en la siguiente stack[self.count] = NUEVO_VALOR O(1)
+print(stack)
+print(stack.pop())  # Elimina stack[len(stack) - 1] O(1)
+print(stack)
+
+# Lista enlazada --> self.head ... self.tail
+
+# Pila
+
+# Listas enlazadas
+            # Tiempo    #       Espacio
+# Push -->          O(1)           O(1)
+# Pop -->           O(1)            O(1)
+# Size -->          O(1)           O(1)
+# Top/Peek -->      O(1)           O(1)
+
+# Arreglos              Tiempo      Espacio
+# Push -->              O(1)        O(1)
+# Pop -->               O(1)        O(1)       
+# Size -->              O(1)        O(1)
+# Top/Peek -->          O(1)        O(1)
