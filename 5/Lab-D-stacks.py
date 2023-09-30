22112b0195ed46cdb42c278f404413659d41ea46
zalogarciam <gegarciam95@gmail.com>
2023-09-30 10:26:55 -0500

    Lab D stacks

--- 5/Lab-D-stacks.py
+++ 5/Lab-D-stacks.py
@@ -124,9 +124,112 @@ stack.push(3)
 stack.push(5)
 stack.push(2)
 stack.push(7)
-stack.print_stack()
-print(stack.pop())
-stack.print_stack()
-print(stack.is_empty())
-print(stack.size())
-print(stack.peek())
+# stack.print_stack()
+# print(stack.pop())
+# stack.print_stack()
+# print(stack.is_empty())
+# print(stack.size())
+# print(stack.peek())
+
+class ArrayStack():
+    def __init__(self) -> None:
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    
+    def print_stack(self):
+        print(self.items)
+
+array_stack = ArrayStack()
+array_stack.push(3)
+array_stack.push(5)
+array_stack.push(2)
+array_stack.push(7)
+# array_stack.print_stack()
+array_stack.pop()
+# array_stack.print_stack()
+# print(array_stack.size())
+# print(array_stack.is_empty())
+# print(array_stack.peek())
+
+# Ejercicio 2
+
+word= "EXTINTOR"
+
+stack = []
+
+for character in word:
+    stack.append(character)
+
+reverted_word = ''
+while len(stack) >= 1:
+    reverted_word += stack.pop()
+
+# print(reverted_word )
+
+
+
+# Ejercicio 4
+
+input = ['2', '3', '1', '*', '+', '9', '-']
+
+stack = []
+for element in input:
+    if element.isdigit(): 
+        stack.append(element)
+    else:
+        elem1 = stack.pop()
+        elem2 = stack.pop()
+        result = eval(elem2 + element + elem1)
+        stack.append(str(result))
+# print(stack)
+
+
+# Ejercicio 3
+
+def parenthesis(input):
+    stack = []
+    if input[0] == ")": return False
+    for parenthesis in input:
+        if parenthesis == "(":
+            stack.append(parenthesis)
+        else:
+            stack.pop()
+    return len(stack) == 0
+
+# print(parenthesis("(())"))
+# print(parenthesis("(()())"))
+# print(parenthesis("(()"))
+# print(parenthesis(")()"))
+
+
+def decimal_to_binary(number):
+    stack = []
+    result = ""
+    while number >= 0:
+        res = number % 2 
+        stack.append(res)
+        number = number // 2
+        if number == 0 or number == 1:
+            stack.append(number)
+            break
+    
+    while len(stack) > 0:
+        result += str(stack.pop())
+    return result
+
+print(decimal_to_binary(345))
+
