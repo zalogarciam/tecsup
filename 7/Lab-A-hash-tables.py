2b5f148cce8a01dd85ee06682563ff06c472474f
zalogarciam <gegarciam95@gmail.com>
2023-10-11 21:15:08 -0500

    Hash table A

--- 7/Lab-A-hash-tables.py
+++ 7/Lab-A-hash-tables.py
@@ -14,7 +14,17 @@ class HashTable():
     def put(self, key, value):
         key_value = KeyValue(key, value)
         hash_index = self.hash(key)
-        self.items[hash_index] = key_value
+        if self.items[hash_index] == None:
+            self.items[hash_index] = key_value
+        else:
+            while self.items[hash_index] != None:
+                hash_index += 1
+                if self.items[hash_index] != None:
+                    continue
+                if hash_index >= self.size:
+                    hash_index = 0
+                self.items[hash_index] = key_value
+                break
 
     def get(self, key):
         hash_index = self.hash(key)
@@ -31,4 +41,23 @@ hash_table = HashTable(10)
 hash_table.put(44, "Jorge")
 hash_table.put(88, "Juan")
 hash_table.put(55, "Maria")
-hash_table.print()
+hash_table.put(14, "Roy")
+# hash_table.print()
+
+# Ejercicio 4
+
+dict = {"Ed":17 , "Roy": 23, "Edgar":27, "Juan":19, "Jean":18}
+
+maximum = -999999
+minimum = 999999
+key_max = None
+key_min = None
+for persona in dict:
+    if dict[persona] > maximum:
+        maximum = dict[persona]
+        key_max = persona
+    if dict[persona] < minimum:
+        minimum = dict[persona]
+        key_min = persona
+print(key_max)
+print(key_min)
