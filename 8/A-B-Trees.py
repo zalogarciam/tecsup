19afa20536d43beba72385b356c47764a642cd56
zalogarciam <gegarciam95@gmail.com>
2023-10-17 20:06:06 -0500

    Tree class node

---
+++ 8/A-B-Trees.py
@@ -0,0 +1,15 @@
+class Node():
+    def __init__(self, value) -> None:
+        self.value = value
+        self.right = None
+        self.left = None
+
+class Tree():
+    def __init__(self) -> None:
+        self.root = None
+
+tree = Tree()
+root_node = Node(8)
+tree.root = root_node
+tree.root.left = Node(3)
+tree.root.right = Node(10)
