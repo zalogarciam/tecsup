07b4f15f90e639f2421f77453316a396e9a379ae
zalogarciam <gegarciam95@gmail.com>
2023-10-18 21:11:01 -0500

    insert reecursion lab A

--- 8/Lab-A-trees.py
+++ 8/Lab-A-trees.py
@@ -25,21 +25,42 @@ class BinarySearchTree():
                         break
                     current = current.right
 
+    def insert_(self, root, value):
+        if root is None:
+            return Node(value)
+        if value > root.data:
+            root.right = self.insert_(root.right, value)
+        else:
+            root.left = self.insert_(root.left, value)
+        return root
+
     def in_order(self, root, level = 1):
         if root is not None:
             self.in_order(root.left, level + 1)
             print(level * 4 * "-" + str(root.data))
             self.in_order(root.right, level + 1)
 
+    def pre_order(self, root, level = 1):
+        if root is not None:
+            print(str(root.data))
+            self.pre_order(root.left, level + 1)
+            self.pre_order(root.right, level + 1)
+
+    def post_order(self, root, level = 1):
+        if root is not None:
+            self.post_order(root.left, level + 1)
+            self.post_order(root.right, level + 1)
+            print(str(root.data))
 
 bst = BinarySearchTree()
-bst.insert(4)
-bst.insert(2)
-bst.insert(6)
-bst.insert(1)
-bst.insert(3)
-bst.insert(5)
-bst.in_order(bst.root)
+tmp = bst.insert_(bst.root, 4)
+tmp = bst.insert_(tmp, 2)
+tmp = bst.insert_(tmp, 6)
+tmp = bst.insert_(tmp, 1)
+tmp = bst.insert_(tmp, 3)
+tmp = bst.insert_(tmp, 5)
+tmp = bst.insert_(tmp, 4.5)
+bst.post_order(tmp)
 
 # bst_root = Node(10)
 # bst_root.left = Node(7)
