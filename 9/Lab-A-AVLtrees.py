fe9cd88ed9dc63fda98e952735a7a424e8ab24a6
zalogarciam <gegarciam95@gmail.com>
2023-10-25 21:09:14 -0500

    AVL trees

--- 9/Lab-A-AVLtrees.py
+++ 9/Lab-A-AVLtrees.py
@@ -16,13 +16,32 @@ class AVLTree():
             root.right = self.insert(root.right, value)
         else:
             root.left = self.insert(root.left, value)
-        return root
+        root.height = self.set_height(root)
+        return self.balance(root)
     
-    def rotate_left():
-        pass
+    def rotate_left(self, root):
+        new_root = root.right
+        root.right = new_root.left
+        new_root.left = root
+        self.set_height(root)
+        self.set_height(new_root)
+        return new_root
 
-    def rotate_right():
-        pass
+    def rotate_right(self, root):
+        new_root = root.left
+        root.left = new_root.right
+        new_root.right = root
+        self.set_height(root)
+        self.set_height(new_root)
+        return new_root
+
+    def height(self, node):
+        if node is None:
+            return -1
+        return max(self.height(node.left), self.height(node.right)) + 1
+    
+    def set_height(self, node):
+        node.height = max(self.height(node.left), self.height(node.right)) + 1
 
     def in_order(self, root, level = 1):
         if root is not None:
@@ -30,11 +49,33 @@ class AVLTree():
             print(level * 4 * "-" + str(root.data))
             self.in_order(root.right, level + 1)
 
+    def left_heavy(self, node):
+        return self.balance_factor(node) > 1
+
+    def right_heavy(self, node):
+        return self.balance_factor(node) < -1
+
+    def balance_factor(self, node):
+        if node is None: return 0
+        else: return self.height(node.left) - self.height(node.right)
+
+    def balance(self, root):
+        if self.left_heavy(root):
+            if self.balance_factor(root.left) < 0:
+                root.left = self.rotate_left(root.left)
+            return self.rotate_right(root)
+        elif self.right_heavy(root):
+            if self.balance_factor(root.right) > 0:
+                root.right = self.rotate_right(root.right)
+            return self.rotate_left(root)
+        return root
+    
 tree = AVLTree()
-tree.root = AVLNode(12)
+tree.insert(tree.root, 12)
+
 tree.insert(tree.root, 3)
 tree.insert(tree.root, 9)
-tree.insert(tree.root, 4)
-tree.insert(tree.root, 6)
-tree.insert(tree.root, 2)
+# tree.insert(tree.root, 4)
+# tree.insert(tree.root, 6)
+# tree.insert(tree.root, 2)
 tree.in_order(tree.root)
