1b5045e3d7cf057e259fd5a1167a95a4e92c7ca7
zalogarciam <gegarciam95@gmail.com>
2023-11-08 21:13:20 -0500

    Finishing Trie for LAB A

--- 11/Lab-A-Tries.py
+++ 11/Lab-A-Tries.py
@@ -9,7 +9,8 @@ class Node():
 
 class Trie():
     def __init__(self) -> None:
-        self.root = Node("NULL")
+        self.root = Node("ROOT")
+        self.count = 0
 
     def insert(self, string):
         current = self.root
@@ -18,9 +19,77 @@ class Trie():
                 current.children[char] = Node(char)
             current = current.children[char]
         current.eow = True
+        self.count += 1
 
     def traverse(self):
         self.traverse_(self.root)
 
+    #post order
     def traverse_(self, node):
-        pass
+        for item in node.children.values():
+            self.traverse_(item)
+        print(node.data)
+
+    def pprint(self):
+        self.pprint_(self.root, "")
+
+    def pprint_(self, node, prefix):
+        if node is None: return
+        if node.eow:
+            print(prefix + "|--", node.data, '(eow)')
+        else:
+            print(prefix + "|--", node.data)
+        prefix = prefix + "|    "
+        for child in node.children:
+            self.pprint_(node.children[child], prefix)
+
+    # def traverse_(self, node):
+    #     print(node.data)
+    #     for item in node.children.values():
+    #         self.traverse_(item)
+
+    def search(self, word):
+        current = self.root
+        if word is None: return False
+        for char in word:
+            if char not in current.children:
+                return False
+            else:
+                current = current.children[char]
+        return current.eow
+
+    def remove(self, word):
+        if word is None: return
+        self.remove_(self.root, word, 0)
+
+    def remove_(self, node, word, index):
+        if index == len(word):
+            node.eow = False
+            return
+        char = word[index] # c , a , r , e ...
+        child = node.children.get(char, None)
+        self.remove_(child, word, index + 1)
+        if len(child.children) == 0 and not child.eow:
+            node.children.pop(char)
+
+    def count_trie(self):
+        return self.count
+
+    def count_words(self):
+        return self.count_words_(self.root)
+
+    def count_words_(self, node):
+        count = 0 
+        if node.eow: count +=1
+        for item in node.children.values():
+            count += self.count_words_(item)
+        return count
+
+trie = Trie()
+trie.insert('cat')
+trie.insert('car')
+trie.insert('care')
+trie.insert('careful')
+trie.remove('careful')
+print(trie.count_words())
+trie.pprint()
